c3fa215581529e7452a60f88c88d5e93175b769a
Voldezhur <voldezhur@gmail.com>
2024-11-02 09:42:17 +0300

    Began switching to using a string instead of a cumbersome list

--- scientific_calculator/lib/pages/home_page.dart
+++ scientific_calculator/lib/pages/home_page.dart
@@ -13,98 +13,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List equation = <String>[];
-  String equationString = '';
-  String displayString = '';
+  String equation = '';
 
   final List numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 
   void _addToEquation(EquationItem element) {
     setState(() {
-      // Если последний элемент в выражении - число, то просто прибавляем цифру к нему
-      if (equation.isNotEmpty &&
-          isNumeric(equation.last) &&
-          isNumeric(element.string)) {
-        equation.last += element.string;
-      }
-
-      // Иначе добавляем новый элемент в конец списка
-      else {
-        equation.insert(equation.length, element.string);
-      }
-
-      // Обновляем строку на выводе
-      equationString = equation.join();
+      equation += element.string;
     });
   }
 
   void _removeFromEquation() {
     setState(() {
-      if (equation.isNotEmpty && equation.isNotEmpty) {
-        // Если последний элемент списка - число, то просто удаляем цифру от него
-        if (isNumeric(equation.last)) {
-          equation.last = equation.last.substring(0, equation.last.length - 1);
-        }
-
-        // Иначе удаляем последний элемент списка
-        else {
-          equation.removeLast();
-        }
-
-        // Обновляем строку на выводе
-        equationString = equation.join();
-      }
+      equation = equation.substring(0, equation.length - 1);
     });
   }
 
   void _clearEquation() {
     setState(() {
-      equation = [];
-      equationString = equation.join();
+      equation = "";
     });
-  }
-
-  bool isNumeric(String s) {
-    return double.tryParse(s) != null;
-  }
-
-  String _preprocessEquation(List equation) {
-    // Проходимся по всем элементам массива
-    for (var i = 0; i < equation.length; i++) {
-      // Если нашли функцию
-      if (!isNumeric(equation[i]) && equation[i].length > 1) {
-        String func = equation[i];
-
-        // Если функция принимает один аргумент
-        if (func[0] == '1') {
-          // Обрезаем количество аргументов
-          func = func.substring(1, func.length);
-
-          // Берем следующее число в качестве аргумента
-          if (i + 1 < equation.length) {
-            // Вводим аргумент под скобки
-            func = func.substring(0, func.length - 2) + equation[i + 1] + ')';
-
-            // Удаляем лишнее число из выражения
-            equation[i] = func;
-            equation.removeAt(i + 1);
-          }
-        }
-
-        // Если функция принимает два аргумента
-        if (func[0] == '2') {}
-      }
-    }
-
-    return equation.join();
   }
 
   void _displayResult() {
-    setState(() {
-      equationString = _preprocessEquation(equation);
-      Expression exp = Expression(equationString);
-      equationString = exp.eval().toString();
-    });
+    setState(() {});
   }
 
   @override
@@ -121,7 +53,7 @@ class _HomePageState extends State<HomePage> {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Text(
-                  equationString,
+                  equation,
                   style: const TextStyle(fontSize: 26),
                 ),
                 Row(
