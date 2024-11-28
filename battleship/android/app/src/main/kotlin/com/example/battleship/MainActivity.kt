6be081fc1542a9ad5a4e2d1af193db87282ce268
Voldezhur <voldezhur@gmail.com>
2024-11-28 17:05:59 +0300

    Created new flutter project and edited readme

--- battleship/android/app/src/main/kotlin/com/example/battleship/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.battleship
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ knighthood/android/app/src/main/kotlin/com/example/knighthood/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.knighthood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
