ad9f8a7631aba58883e041d541deb7c74d9cd370
Voldezhur <voldezhur@gmail.com>
2024-12-05 16:57:10 +0300

    Beginning to implement the A* search algorithm for the enemies

--- knighthood/lib/globals/entities.dart
+++ knighthood/lib/globals/entities.dart
@@ -5,3 +5,9 @@ import 'package:knighthood/models/entity.dart';
 // Вторая координата - Y
 Entity player = Entity('player', 4, 2, -1, -1, false);
 Entity boar = Entity('boar', 0, 0, -1, -1, true);
+
+// Функции энтити
+// Функция поиска ближейшего пути энтити до игрока
+// Алгоритм A*
+// https://www.geeksforgeeks.org/a-search-algorithm/
+
---
+++ knighthood/lib/models/cell.dart
@@ -0,0 +1,39 @@
+// Класс для алгоритма A*
+// https://www.geeksforgeeks.org/a-search-algorithm/
+import 'package:knighthood/globals/entities.dart';
+import 'package:knighthood/globals/game_state.dart';
+
+class Cell {
+  int parentY;
+  int parentX;
+
+  int F;
+  int g;
+  int h;
+
+  Cell(
+    this.parentY,
+    this.parentX, {
+    this.F = 0,
+    this.g = 0,
+    this.h = 0,
+  });
+}
+
+// При работе с картами, координаты в порядке YX, а не XY
+
+// Проверка, что клетка в пределах карты
+bool isValid(y, x) {
+  int mapSize = currentMap.layout.length;
+  return (y >= 0) && (y < mapSize) && (x >= 0) && (x < mapSize);
+}
+
+// Проверка, что клетка является целью
+bool isDestination(y, x) {
+  return (y == player.posY) && (x == player.posX);
+}
+
+// Манхеттенское расстояние
+int manhattan(y, x) {
+  return (y - player.posY).abs() + (x - player.posX).abs();
+}
